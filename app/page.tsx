e78badc299e68b1eacf8a7cbe0975f9220ab72b8
masakrambutt <msakrambutt@gmail.com>
2023-01-09 19:22:28 +0500

    update2:

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h3 className='text-2xl font-semibold'> Hello World from Shahid Akram Butt</h3>
+      <h3 className='text-2xl font-semibold'> Hello Dear, world from Shahid Akram Butt</h3>
       <br/>
           <Link href="/name">Go to name page</Link>
     </div>     
